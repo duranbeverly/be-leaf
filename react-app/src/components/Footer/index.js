2be33a045a45cfcfc548d3892cdea25ec93653c7
Beverly <duran.beverly@gmail.com>
2023-06-14 02:58:37 -0400

    fixed links

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -9,12 +9,12 @@ export default function Footer() {
             <div className='footer'>
                 <p className='developer-name'>Beverly Duran</p>
                 <div className='social-media-div'>
-                    <NavLink path to="https://github.com/duranbeverly">
+                    <a href="https://github.com/duranbeverly" target="_blank">
                         <img className="social-icon" src="https://be-leaf.s3.amazonaws.com/github-icon.png"></img>
-                    </NavLink>
-                    <NavLink path to="https://www.linkedin.com/in/beverly-duran/">
+                    </a>
+                    <a href="https://www.linkedin.com/in/beverly-duran/" target="_blank">
                         <img className="social-icon" src="https://be-leaf.s3.amazonaws.com/linkedin-icon.png"></img>
-                    </NavLink>
+                    </a>
                 </div>
 
             </div>
