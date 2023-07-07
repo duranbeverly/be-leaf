23ab859122be16cd23d58a854f3415137c841474
Beverly <duran.beverly@gmail.com>
2023-07-07 10:11:02 -0400

    scroll effects and loading

--- react-app/src/components/PlantList/index.js
+++ react-app/src/components/PlantList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPlants } from "../../store/plants";
 import { thunkAddFav, thunkDeleteFav } from "../../store/session";
@@ -11,6 +11,7 @@ export default function PlantList() {
     let [isLoading, setIsLoading] = useState(true)
     let userInfo = useSelector((state) => state.session.user)
     let history = useHistory()
+    const cardRefs = useRef([]);
     // note - in user we have a favorites key that is a dictionary that has all the favorites in it with id as the key
 
     useEffect(() => {
@@ -18,6 +19,37 @@ export default function PlantList() {
     }, [dispatch])
 
     let plants = (useSelector((state) => state.plants.all_plants))
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const visibleHeight = windowHeight + scrollTop;
+
+            cardRefs.current.forEach((cardRef, index) => {
+                const rect = cardRef.getBoundingClientRect();
+                const cardTop = rect.top + scrollTop;
+                const cardBottom = cardTop + rect.height;
+
+                if (cardBottom >= scrollTop && cardTop <= visibleHeight) {
+                    if (cardTop >= scrollTop + windowHeight * 0.8) {
+                        cardRef.classList.add("fade-in");
+                    } else {
+                        cardRef.classList.remove("fade-in");
+                    }
+                } else {
+                    cardRef.classList.remove("fade-in");
+                }
+            });
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        handleScroll(); // Call handleScroll once on initial render to apply fade-in effect to the visible items
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     if (isLoading) {
         return <div className="plant-index-container" style={{ opacity: 0.5 }}>
             <ClockLoader color="#224229" size={30} />
@@ -30,7 +62,7 @@ export default function PlantList() {
         <div className="plant-index-container">
             <h1 className="plant-index-title">Indoor Plants</h1>
             <div className="all-plant-cards">
-                {plants && Object.values(plants).map(plant => {
+                {plants && Object.values(plants).map((plant, index) => {
                     let image = plant.preview_image
                     let name = plant.name
                     let user = plant.first_name
@@ -38,7 +70,7 @@ export default function PlantList() {
                     let plantId = parseInt(plant.id)
 
                     return (
-                        <div className="plant-card" key={plant.id} >
+                        <div className="plant-card" key={plant.id} ref={(ref) => (cardRefs.current[index] = ref)} >
                             <NavLink className="plant-index-card" to={`/plants/${plantId}`}>
                                 <div className="img-container">
 
